20a12f972eeccce52303d9a309538c8e812ec022
Amanda Jensen <jens2482@umn.edu>
2024-01-26 21:01:21 -0600

    New dataset Female cases only

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,5 @@
 #Import and Labeling
 rt_df <- read.csv (file = 'data/week2.csv', header = TRUE)
+rt_df$condition <- factor(rt_df$condition, labels = c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, labels = c("Female", "Male", "Nonbinary"))
+
