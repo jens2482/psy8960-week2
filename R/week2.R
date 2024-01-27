970cbf2a254d8d732e442ea6375f3f2657b8e2c0
Amanda Jensen <jens2482@umn.edu>
2024-01-26 21:14:38 -0600

    Histogram, list, and column display

--- R/week2.R
+++ R/week2.R
@@ -3,3 +3,9 @@ rt_df <- read.csv (file = 'data/week2.csv', header = TRUE)
 rt_df$condition <- factor(rt_df$condition, labels = c("Control", "Experimental"))
 rt_df$gender <- factor(rt_df$gender, labels = c("Female", "Male", "Nonbinary"))
 
+#Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, subset = gender == "Female")
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]][2]
